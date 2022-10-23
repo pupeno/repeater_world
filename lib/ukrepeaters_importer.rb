a0fa6341e36299dd29d70881fe9fe969043b5202
Pablo Fernandez <pupeno@pupeno.com>
2022-10-23 09:22:50 +0100

    Start loading dv_modes_only.csv and some refactoring.

--- lib/ukrepeaters_importer.rb
+++ lib/ukrepeaters_importer.rb
@@ -3,48 +3,64 @@ class UkrepeatersImporter
     # TODO: uncomment this
     # download_all_files
 
-    voice_repeater_list = CSV.table("./tmp/ukrepeaters/voice_repeater_list.csv")
-    assert_fields(voice_repeater_list, [:repeater, :band, :channel, :tx, :rx, :mode, :qthr, :where, :region, :code, :keeper, :lat, :lon, nil])
-
     puts "Creating repeaters..."
     Repeater.transaction do
+      voice_repeater_list = CSV.table("./tmp/ukrepeaters/voice_repeater_list.csv")
+      assert_fields(voice_repeater_list, [:repeater, :band, :channel, :tx, :rx, :mode, :qthr, :where, :region, :code, :keeper, :lat, :lon, nil])
+
       voice_repeater_list.each_with_index do |raw_repeater, line_number|
-
-        # For the UK, we treate the call sign as unique and the identifier of the repeater.
-        repeater = Repeater.find_or_initialize_by(call_sign: raw_repeater[:repeater].upcase)
-
-        repeater.name = raw_repeater[:where].titleize
-        repeater.band = raw_repeater[:band].downcase
-        repeater.channel = raw_repeater[:channel]
-        repeater.keeper = raw_repeater[:keeper]
-
-        repeater.tx_frequency = raw_repeater[:tx].to_f * 10 ** 6
-        repeater.rx_frequency = raw_repeater[:rx].to_f * 10 ** 6
-        if raw_repeater[:code].present?
-          repeater.access_method = Repeater::CTCSS
-          repeater.ctcss_tone = raw_repeater[:code]
-          repeater.tone_sql = false # TODO: how do we know when this should be true?
-        else
-          repeater.access_method = Repeater::TONE_BURST
-        end
-
-        repeater.grid_square = raw_repeater[:qthr].upcase
-        repeater.latitude = raw_repeater[:lat]
-        repeater.longitude = raw_repeater[:lon]
-        repeater.country_id = "gb"
-        repeater.region_1 = raw_repeater[:region]
-
-        repeater.save!
-
-        puts "  Created #{repeater}."
-      rescue => e
+        create_repeater(raw_repeater)
+      rescue
         raise StandardError.new("Failed to import record on #{line_number + 2}: #{raw_repeater.to_s}") # Line numbers start at 1, not 0, and there's a header, hence the +2
       end
+
+      dv_modes_only = CSV.table("./tmp/ukrepeaters/dv_modes_only.csv")
+      assert_fields(dv_modes_only, [:call, :band, :chan, :txmhz, :rxmhz, :ctcss, :loc, :where, :dmr, :dstar, :fusion, :nxdn, nil])
     end
     puts "Done creating repeaters."
   end
 
   private
+
+  # Create repeater from a record in voice_repeater_list.csv
+  def create_repeater(raw_repeater)
+    # For the UK, we treat the call sign as unique and the identifier of the repeater.
+    repeater = Repeater.find_or_initialize_by(call_sign: raw_repeater[:repeater].upcase)
+    already_existed = !repeater.new_record?
+
+    # Some metadata.
+    repeater.name = raw_repeater[:where].titleize
+    repeater.band = raw_repeater[:band].downcase
+    repeater.channel = raw_repeater[:channel]
+    repeater.keeper = raw_repeater[:keeper]
+
+    # How to access the repeater.
+    repeater.tx_frequency = raw_repeater[:tx].to_f * 10 ** 6
+    repeater.rx_frequency = raw_repeater[:rx].to_f * 10 ** 6
+    if raw_repeater[:code].present?
+      repeater.access_method = Repeater::CTCSS
+      repeater.ctcss_tone = raw_repeater[:code]
+      repeater.tone_sql = false # TODO: how do we know when this should be true?
+    else
+      repeater.access_method = Repeater::TONE_BURST
+    end
+
+    # The location of the repeater
+    repeater.grid_square = raw_repeater[:qthr].upcase
+    repeater.latitude = raw_repeater[:lat]
+    repeater.longitude = raw_repeater[:lon]
+    repeater.country_id = "gb"
+    repeater.region_1 = raw_repeater[:region]
+
+    repeater.save!
+
+    if already_existed
+      puts "  Already existed: #{repeater}"
+    else
+      puts "  Created #{repeater}."
+    end
+    repeater
+  end
 
   def download_all_files
     puts "Downloading files..."
@@ -73,7 +89,7 @@ class UkrepeatersImporter
 
   def assert_fields(table, fields)
     if table.headers != fields
-      raise "The fields for voice_repeater_list.csv changed, so we can't process it.\n  Expected: #{fields.inspect}\n  Received: #{voice_repeater_list.headers.inspect}"
+      raise "The fields for voice_repeater_list.csv changed, so we can't process it.\n  Expected: #{fields.inspect}\n  Received: #{table.headers.inspect}"
     end
   end
 end
