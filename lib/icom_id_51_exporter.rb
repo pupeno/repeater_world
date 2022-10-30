4b3c4710a136920d8674dee8d6fb5fa8d19d87d3
Pablo Fernandez <pupeno@pupeno.com>
2022-10-30 19:04:01 +0000

    Add exporting to ID-51.

--- lib/icom_id_51_exporter.rb
+++ lib/icom_id_51_exporter.rb
@@ -1,2 +1,9 @@
-class IcomId51Exporter < IcomId52AndId52Exporter
+class IcomId51Exporter < IcomId51AndId52Exporter
+  def dstar_repeater(repeater)
+    row = super(repeater)
+
+    row["TONE"] = "OFF" # It's D-Star, of course it's off. But ID-51 seems to want it this way.
+    row["Repeater Tone"] = "88.5Hz" # ID-51 insists it wants this to be 88.5Hz. The ID-52 has a similar broken behaviour.
+    row
+  end
 end
