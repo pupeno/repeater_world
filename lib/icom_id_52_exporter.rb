9adda1e552413242de90d0cb71b384564b5541b0
Pablo Fernandez <pupeno@pupeno.com>
2022-10-30 09:06:44 +0000

    Non-semantic: refactoring.

--- lib/icom_id_52_exporter.rb
+++ lib/icom_id_52_exporter.rb
@@ -12,35 +12,41 @@ class IcomId52Exporter < Exporter
         .find_each do |repeater|
         if [Repeater::BAND_2M, Repeater::BAND_70CM].include? repeater.band
           if repeater.fm?
-            csv << [
-              1, # TODO: do something smarter about groups.
-              repeater.country&.name, # TODO: do something smarter about groups.
-              repeater.name,
-              "", # TODO: what's this?
-              repeater.call_sign,
-              "", # TODO: what's this?
-              "%.6f" % (repeater.tx_frequency.to_f / 10 ** 6),
-              repeater.tx_frequency > repeater.rx_frequency ? "DUP-" : "DUP+",
-              "%.6f" % ((repeater.tx_frequency - repeater.rx_frequency).to_f / 10 ** 6),
-              "FM",
-              case repeater.access_method
-                when Repeater::TONE_BURST
-                  "OFF"
-                when Repeater::CTCSS
-                  "TONE" # TODO: when do we use TSQL
-              end,
-              "#{repeater.ctcss_tone}Hz",
-              "Yes", # TODO: what is this?
-              "Approximate", # TODO: why does the export have some "Exacts"
-              repeater.latitude,
-              repeater.longitude,
-              "--:--" # TODO: export UTC Offset.
-            ]
+            csv << fm_repeater(repeater)
           elsif repeater.dstar?
             csv << ["TODO: export dstar"]
           end
         end
       end
     end
+  end
+
+  private
+
+  def fm_repeater(repeater)
+    [
+      1, # TODO: do something smarter about groups.
+      repeater.country&.name, # TODO: do something smarter about groups.
+      repeater.name,
+      "", # TODO: what's this?
+      repeater.call_sign,
+      "", # TODO: what's this?
+      "%.6f" % (repeater.tx_frequency.to_f / 10 ** 6),
+      repeater.tx_frequency > repeater.rx_frequency ? "DUP-" : "DUP+",
+      "%.6f" % ((repeater.tx_frequency - repeater.rx_frequency).to_f / 10 ** 6),
+      "FM",
+      case repeater.access_method
+        when Repeater::TONE_BURST
+          "OFF"
+        when Repeater::CTCSS
+          "TONE" # TODO: when do we use TSQL
+      end,
+      "#{repeater.ctcss_tone}Hz",
+      "Yes", # TODO: what is this?
+      "Approximate", # TODO: why does the export have some "Exacts"
+      repeater.latitude,
+      repeater.longitude,
+      "--:--" # TODO: export UTC Offset.
+    ]
   end
 end
