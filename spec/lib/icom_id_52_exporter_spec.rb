903465028476e7fe2b3c9f9844cc7a86c689b0dc
Pablo Fernandez <pupeno@pupeno.com>
2022-10-30 17:04:13 +0000

    Increase code coverage to 100% for ID-52 export.

--- spec/lib/icom_id_52_exporter_spec.rb
+++ spec/lib/icom_id_52_exporter_spec.rb
@@ -14,6 +14,16 @@ RSpec.describe IcomId52Exporter do
         nxdn: nil,
         grid_square: "IO80SN", latitude: 50.55, longitude: -2.44, country_id: "gb", region_1: "England", region_2: "South West", region_3: "DT5", region_4: "Weymouth", source: "ukrepeater.net"),
       create(:repeater,
+        name: "Herne Bay", call_sign: "GB7IC-C", band: "2m", channel: "RV53", keeper: "G4TKR",
+        operational: true, notes: nil,
+        tx_frequency: 145662500, rx_frequency: 145062500,
+        fm: true, access_method: nil, ctcss_tone: nil, tone_sql: nil,
+        dstar: true,
+        fusion: false,
+        dmr: false, dmr_cc: nil, dmr_con: nil,
+        nxdn: nil,
+        grid_square: "JO01NI", latitude: 51.36, longitude: 1.15, country_id: "gb", region_1: "England", region_2: "South East", region_3: "CT6", region_4: "Herne Bay", source: "ukrepeater.net"),
+      create(:repeater,
         name: "Dufton", call_sign: "GB3VE", band: "70cm", channel: "RB04", keeper: "G0TNF",
         operational: false, notes: nil,
         tx_frequency: 433100000, rx_frequency: 434700000,
@@ -34,7 +44,8 @@ RSpec.describe IcomId52Exporter do
         dmr: true, dmr_cc: 1, dmr_con: "BRANDMEISTER",
         nxdn: true,
         grid_square: "IO92GW", latitude: 52.9, longitude: -1.4, country_id: "gb", region_1: "England", region_2: "Midlands", region_3: "DE21", region_4: "Derby",
-        source: "ukrepeater.net")
+        source: "ukrepeater.net"),
+      create(:repeater, call_sign: nil)
     ]
 
     exporter = IcomId52Exporter.new(Repeater.all)
@@ -44,6 +55,9 @@ RSpec.describe IcomId52Exporter do
       1,United Kingdom,Derby,England,GB7DC,,430.875000,DUP+,7.600000,FM,TONE,71.9Hz,YES,Approximate,52.900000,-1.400000,--:--
       1,United Kingdom,Derby,England,GB7DC  B,GB7DC  G,430.875000,DUP+,7.600000,DV,,,YES,Approximate,52.900000,-1.400000,--:--
       1,United Kingdom,Dufton,England,GB3VE  B,GB3VE  G,433.100000,DUP+,1.600000,DV,,,YES,Approximate,54.680000,-2.460000,--:--
+      1,United Kingdom,Herne Bay,No CTCSS,GB7IC C,,145.662500,DUP-,0.600000,FM,OFF,88.5Hz,YES,Approximate,51.360000,1.150000,--:--
+      1,United Kingdom,Herne Bay,England,GB7IC  C,GB7IC  G,145.662500,DUP-,0.600000,DV,,,YES,Approximate,51.360000,1.150000,--:--
+      1,United Kingdom,Repeater,WM,,,144.962500,DUP-,0.600000,FM,OFF,88.5Hz,YES,Approximate,51.740000,-3.420000,--:--
       1,United Kingdom,Weymouth,No CTCSS,GB3DR,,145.737500,DUP-,0.600000,FM,OFF,88.5Hz,YES,Approximate,50.550000,-2.440000,--:--
     CSV
   end
