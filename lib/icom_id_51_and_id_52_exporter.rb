f703c3e96fe3e36156daa5ff86acaf62c6283da8
Pablo Fernandez <pupeno@pupeno.com>
2022-10-30 18:47:38 +0000

    Use hashes instead of arrays to represent rows.

--- lib/icom_id_51_and_id_52_exporter.rb
+++ lib/icom_id_51_and_id_52_exporter.rb
@@ -21,49 +21,29 @@ class IcomId51AndId52Exporter < Exporter
 
   private
 
-  # Max lengths for each field is defined on page iv of the Icom ID-52 Advance Manual
+  # Max lengths for each field is defined on page iv of the Icom ID-52 Advance Manual. We are assuming it's the same for
+  # the ID-51, but if not, this might need changing.
   MAX_COUNTRY_NAME_LENGTH = 16
   MAX_NAME_LENGTH = 16
   MAX_SUB_NAME_LENGTH = 8
   MAX_CALL_SIGN_LENGTH = 8
   MAX_GATEWAY_CALL_SIGN_LENGTH = 8
 
-  def fm_repeater(repeater)
+  def repeater(repeater)
     group_name = truncate(MAX_COUNTRY_NAME_LENGTH, repeater.country.name) # TODO: do something smarter about groups.
     name = truncate(MAX_NAME_LENGTH, repeater.name)
-    sub_name = truncate(MAX_SUB_NAME_LENGTH,
-      if repeater.access_method.present?
-        repeater.region_1
-      else
-        "No CTCSS"
-      end)
+    sub_name = repeater.region_1
 
     call_sign = truncate(MAX_CALL_SIGN_LENGTH, repeater.call_sign&.tr("-", " ")) # In the UK, some call signs have a hyphen, but ID-52 doesn't like that.
 
-    tone = case repeater.access_method
-    when Repeater::CTCSS
-      "TONE" # TODO: when do we use TSQL
-    else
-      "OFF" # Repeater::TONE_BURST or NULL is "OFF".
-    end
-    ctcss = case repeater.access_method
-    when Repeater::CTCSS
-      "#{repeater.ctcss_tone}Hz"
-    else
-      "88.5Hz" # ID-52 insists on having some value here, even if it makes no sense and it's not used.
-    end
     {"Group No" => 1, # TODO: do something smarter about groups.
      "Group Name" => group_name,
      "Name" => name,
      "Sub Name" => sub_name,
      "Repeater Call Sign" => call_sign,
-     "Gateway Call Sign" => nil, # FM repeaters don't have a gateway.
      "Frequency" => "%.6f" % (repeater.tx_frequency / 10**6),
      "Dup" => repeater.tx_frequency > repeater.rx_frequency ? "DUP-" : "DUP+",
      "Offset" => "%.6f" % ((repeater.tx_frequency - repeater.rx_frequency).abs / 10**6),
-     "Mode" => "FM",
-     "TONE" => tone,
-     "Repeater Tone" => ctcss,
      "RPT1USE" => "YES", # Yes, we want to use the repeater.
      "Position" => "Approximate", # TODO: why does the export have some "Exacts"
      "Latitude" => "%.6f" % repeater.latitude,
@@ -71,42 +51,52 @@ class IcomId51AndId52Exporter < Exporter
      "UTC Offset" => repeater.utc_offset || "--:--"}
   end
 
+  def fm_repeater(repeater)
+    row = repeater(repeater)
+
+    row["Mode"] = "FM"
+
+    if !repeater.access_method.present?
+      row["Sub Name"] = "No CTCSS"
+    end
+
+    row["TONE"] = case repeater.access_method
+    when Repeater::CTCSS
+      "TONE" # TODO: when do we use TSQL
+    else
+      "OFF" # Repeater::TONE_BURST or NULL is "OFF".
+    end
+
+    row["Repeater Tone"] = case repeater.access_method
+    when Repeater::CTCSS
+      "#{repeater.ctcss_tone}Hz"
+    else
+      "88.5Hz" # ID-52 insists on having some value here, even if it makes no sense and it's not used.
+    end
+
+    row
+  end
+
   # Max lengths for each field is defined on page iv of the Icom ID-52 Advance Manual
   def dstar_repeater(repeater)
-    group_name = truncate(MAX_COUNTRY_NAME_LENGTH, repeater.country.name) # TODO: do something smarter about groups.
-    name = truncate(MAX_NAME_LENGTH, repeater.name)
-    sub_name = truncate(MAX_SUB_NAME_LENGTH, repeater.region_1)
+    row = repeater(repeater)
 
-    call_sign = truncate(MAX_CALL_SIGN_LENGTH,
-      if repeater.call_sign.include? "-"
-        call_sign_and_port = repeater.call_sign.split("-")
-        add_dstar_port(call_sign_and_port.first, call_sign_and_port.second)
-      else
+    row["Mode"] = "DV"
+
+    if repeater.call_sign.include? "-"
+      call_sign_and_port = repeater.call_sign.split("-")
+      row["Repeater Call Sign"] = truncate(MAX_CALL_SIGN_LENGTH,
+        add_dstar_port(call_sign_and_port.first, call_sign_and_port.second))
+    else
+      row["Repeater Call Sign"] = truncate(MAX_CALL_SIGN_LENGTH,
         add_dstar_port(repeater.call_sign,
-          conventional_dstar_port(repeater.band, repeater.country.id))
+          conventional_dstar_port(repeater.band, repeater.country.id)))
+    end
 
-      end)
-
-    gateway_call_sign = truncate(MAX_GATEWAY_CALL_SIGN_LENGTH,
+    row["Gateway Call Sign"] = truncate(MAX_GATEWAY_CALL_SIGN_LENGTH,
       add_dstar_port(repeater.call_sign.split("-").first, "G")) # Always "G" according to page 5-30 of the ID-52 Advanced manual
 
-    {"Group No" => 1, # TODO: do something smarter about groups.
-     "Group Name" => group_name,
-     "Name" => name,
-     "Sub Name" => sub_name,
-     "Repeater Call Sign" => call_sign,
-     "Gateway Call Sign" => gateway_call_sign,
-     "Frequency" => "%.6f" % (repeater.tx_frequency / 10**6),
-     "Dup" => repeater.tx_frequency > repeater.rx_frequency ? "DUP-" : "DUP+",
-     "Offset" => "%.6f" % ((repeater.tx_frequency - repeater.rx_frequency).abs / 10**6),
-     "Mode" => "DV", # D-Star Voice, or Digital Voice
-     "TONE" => nil, # No access method in D-Star,
-     "Repeater Tone" => nil, # No access method in D-Star,
-     "RPT1USE" => "YES", # Yes, we want to use the repeater.
-     "Position" => "Approximate", # TODO: why does the export have some "Exacts"
-     "Latitude" => "%.6f" % repeater.latitude,
-     "Longitude" => "%.6f" % repeater.longitude,
-     "UTC Offset" => repeater.utc_offset || "--:--"}
+    row
   end
 
   def truncate(length, value)
