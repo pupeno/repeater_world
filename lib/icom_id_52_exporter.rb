f5d6eff40ef4935476d10736bf0e15d9db437597
Pablo Fernandez <pupeno@pupeno.com>
2022-10-30 13:51:38 +0000

    Finish exporting so that the file is entirely valid.

--- lib/icom_id_52_exporter.rb
+++ lib/icom_id_52_exporter.rb
@@ -5,11 +5,10 @@ class IcomId52Exporter < Exporter
               "Dup", "Offset", "Mode", "TONE", "Repeater Tone", "RPT1USE", "Position", "Latitude", "Longitude",
               "UTC Offset"]
       @repeaters
-        .where(band: [Repeater::BAND_2M, Repeater::BAND_70CM], # ID-52 can only do VHF and UHF.
-               access_method: [Repeater::TONE_BURST, Repeater::CTCSS]) # TODO: can repeaters have no access method?
+        .where(band: [Repeater::BAND_2M, Repeater::BAND_70CM]) # ID-52 can only do VHF and UHF.
         .where.not(operational: false) # Skip repeaters known to not be operational.
         .where(fm: true).or(Repeater.where(dstar: true)) # ID-52 does FM and DStar only
-        .order(:name)
+        .order(:name, :call_sign)
         .each do |repeater|
         if [Repeater::BAND_2M, Repeater::BAND_70CM].include? repeater.band
           csv << if repeater.fm?
@@ -32,11 +31,16 @@ class IcomId52Exporter < Exporter
   MAX_GATEWAY_CALL_SIGN_LENGTH = 8
 
   def fm_repeater(repeater)
-    group_name = repeater.country&.name&.truncate(MAX_COUNTRY_NAME_LENGTH, omission: "")  # TODO: do something smarter about groups.
-    name = repeater.name.truncate(MAX_NAME_LENGTH, omission: "")
-    sub_name = repeater.region_1.truncate(MAX_SUB_NAME_LENGTH, omission: "")
+    group_name = truncate(MAX_COUNTRY_NAME_LENGTH, repeater.country&.name) # TODO: do something smarter about groups.
+    name = truncate(MAX_NAME_LENGTH, repeater.name)
+    sub_name = truncate(MAX_SUB_NAME_LENGTH,
+                        if repeater.access_method.present?
+                          repeater.region_1
+                        else
+                          "No CTCSS"
+                        end)
 
-    call_sign = repeater.call_sign.truncate(MAX_CALL_SIGN_LENGTH, omission: "")
+    call_sign = truncate(MAX_CALL_SIGN_LENGTH, repeater.call_sign.gsub("-", " ")) # In the UK, some call signs have a hyphen, but ID-52 doesn't like that.
 
     [
       1, # TODO: do something smarter about groups.
@@ -45,35 +49,48 @@ class IcomId52Exporter < Exporter
       sub_name,
       call_sign,
       nil, # FM repeaters don't have a gateway.
-      "%.6f" % (repeater.tx_frequency.to_f / 10 ** 6),
+      "%.6f" % (repeater.tx_frequency / 10 ** 6),
       repeater.tx_frequency > repeater.rx_frequency ? "DUP-" : "DUP+",
-      "%.6f" % ((repeater.tx_frequency - repeater.rx_frequency).to_f / 10 ** 6),
+      "%.6f" % ((repeater.tx_frequency - repeater.rx_frequency).abs / 10 ** 6),
       "FM",
       case repeater.access_method
-        when Repeater::TONE_BURST
-          "OFF"
         when Repeater::CTCSS
           "TONE" # TODO: when do we use TSQL
+        else
+          "OFF" # Repeater::TONE_BURST or NULL is "OFF".
       end,
-      "#{repeater.ctcss_tone}Hz",
-      "Yes", # TODO: what is this?
+      case repeater.access_method
+        when Repeater::CTCSS
+          "#{repeater.ctcss_tone}Hz"
+        else
+          "88.5Hz" # ID-52 insists on having some value here, even if it makes no sense and it's not used.
+      end,
+      "YES", # Yes, we want to use the repeater.
       "Approximate", # TODO: why does the export have some "Exacts"
-      repeater.latitude,
-      repeater.longitude,
+      "%.6f" % repeater.latitude,
+      "%.6f" % repeater.longitude,
       "--:--" # TODO: export UTC Offset.
     ]
   end
 
   # Max lengths for each field is defined on page iv of the Icom ID-52 Advance Manual
   def dstar_repeater(repeater)
-    group_name = repeater.country&.name&.truncate(MAX_COUNTRY_NAME_LENGTH, omission: "")  # TODO: do something smarter about groups.
-    name = repeater.name.truncate(MAX_NAME_LENGTH, omission: "")
-    sub_name = repeater.region_1.truncate(MAX_SUB_NAME_LENGTH, omission: "")
+    group_name = truncate(MAX_COUNTRY_NAME_LENGTH, repeater.country&.name) # TODO: do something smarter about groups.
+    name = truncate(MAX_NAME_LENGTH, repeater.name)
+    sub_name = truncate(MAX_SUB_NAME_LENGTH, repeater.region_1)
 
-    call_sign = add_dstar_port(repeater.call_sign, conventional_dstar_port(repeater.band, repeater.country&.id))
-                  .truncate(MAX_CALL_SIGN_LENGTH, omission: "")
-    gateway_call_sign = add_dstar_port(repeater.call_sign, "G") # Always "G" according to page 5-30 of the ID-52 Advanced manual
-                          .truncate(MAX_GATEWAY_CALL_SIGN_LENGTH, omission: "")
+    call_sign = truncate(MAX_CALL_SIGN_LENGTH,
+                         if repeater.call_sign.include? "-"
+                           call_sign_and_port = repeater.call_sign.split("-")
+                           add_dstar_port(call_sign_and_port.first, call_sign_and_port.second)
+                         else
+                           add_dstar_port(repeater.call_sign,
+                                          conventional_dstar_port(repeater.band, repeater.country&.id))
+
+                         end)
+
+    gateway_call_sign = truncate(MAX_GATEWAY_CALL_SIGN_LENGTH,
+                                 add_dstar_port(repeater.call_sign.split("-").first, "G")) # Always "G" according to page 5-30 of the ID-52 Advanced manual
 
     [
       1, # TODO: do something smarter about groups.
@@ -82,18 +99,21 @@ class IcomId52Exporter < Exporter
       sub_name,
       call_sign,
       gateway_call_sign,
-      "%.6f" % (repeater.tx_frequency.to_f / 10 ** 6),
+      "%.6f" % (repeater.tx_frequency / 10 ** 6),
       repeater.tx_frequency > repeater.rx_frequency ? "DUP-" : "DUP+",
-      "%.6f" % ((repeater.tx_frequency - repeater.rx_frequency).to_f / 10 ** 6),
+      "%.6f" % ((repeater.tx_frequency - repeater.rx_frequency).abs / 10 ** 6),
       "DV",
       nil, # No access method in D-Star,
       nil, # No access method in D-Star,
-      "Yes", # TODO: what is this?
+      "YES", # Yes, we want to use the repeater.
       "Approximate", # TODO: why does the export have some "Exacts"
-      repeater.latitude,
-      repeater.longitude,
+      "%.6f" % repeater.latitude,
+      "%.6f" % repeater.longitude,
       "--:--" # TODO: export UTC Offset.
     ]
   end
 
+  def truncate(length, value)
+    value.truncate(length, omission: "").strip if value
+  end
 end
