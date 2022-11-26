45fb92d086da21c1a5f0accfc1518ee236841bdc
Pablo Fernandez <pupeno@pupeno.com>
2022-11-26 18:17:36 +0000

    Enable disable the geo search.

---
+++ app/javascript/controllers/distance_to_coordinates_controller.js
@@ -0,0 +1,22 @@
+import {Controller} from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["controller", "controlled"]
+
+  connect() {
+    this.controllerTarget.addEventListener("change", this.toggleDisable.bind(this));
+  }
+
+  disconnect() {
+    this.controllerTarget.removeEventListener("change", this.toggleDisable.bind(this));
+  }
+
+  toggleDisable() {
+    const grayedOutText = "text-gray-400";
+    if (this.controllerTarget.checked) {
+      this.controlledTargets.forEach(x => x.classList.remove(grayedOutText))
+    } else {
+      this.controlledTargets.forEach(x => x.classList.add(grayedOutText))
+    }
+  }
+}
