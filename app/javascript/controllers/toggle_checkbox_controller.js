92b69172caa7438844fb35043fb99cfb61d745e3
Pablo Fernandez <pupeno@pupeno.com>
2022-11-26 17:15:40 +0000

    Improve the UX of the toggle checkboxes.

--- app/javascript/controllers/toggle_checkbox_controller.js
+++ app/javascript/controllers/toggle_checkbox_controller.js
@@ -2,12 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.checkbox = this.element.querySelectorAll('input[type=checkbox]')[0];
-    this.label = this.element.querySelectorAll('label')[0];
+    this.checkbox = this.element.querySelector("input[type=checkbox]");
+    this.label = this.element.querySelector("label");
+    this.slot = this.label.querySelector("span.slot");
+    this.circle = this.label.querySelector("span.circle");
 
     this.checkbox.addEventListener("change", this.updateBadge.bind(this));
 
     this.updateBadge()
+    this.slot.classList.remove("bg-orange-400")
+    this.circle.classList.remove("translate-x-2.5")
   }
 
   disconnect() {
@@ -15,16 +19,20 @@ export default class extends Controller {
   }
 
   updateBadge() {
+    const orangeBackground = "bg-orange-800";
+    const grayBackground = "bg-orange-200";
+    const animateRight = "translate-x-5";
+    const animateLeft = "translate-x-0";
     if (this.checkbox.checked) {
-      this.label.querySelectorAll("span")[2].classList.add("bg-indigo-600")
-      this.label.querySelectorAll("span")[2].classList.remove("bg-gray-200")
-      this.label.querySelectorAll("span")[3].classList.add("translate-x-5")
-      this.label.querySelectorAll("span")[3].classList.remove("translate-x-0")
+      this.slot.classList.add(orangeBackground)
+      this.slot.classList.remove(grayBackground)
+      this.circle.classList.add(animateRight)
+      this.circle.classList.remove(animateLeft)
     } else {
-      this.label.querySelectorAll("span")[2].classList.add("bg-gray-200")
-      this.label.querySelectorAll("span")[2].classList.remove("bg-indigo-600")
-      this.label.querySelectorAll("span")[3].classList.add("translate-x-0")
-      this.label.querySelectorAll("span")[3].classList.remove("translate-x-5")
+      this.slot.classList.add(grayBackground)
+      this.slot.classList.remove(orangeBackground)
+      this.circle.classList.add(animateLeft)
+      this.circle.classList.remove(animateRight)
     }
   }
 }
