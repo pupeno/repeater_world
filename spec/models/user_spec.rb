560d10aaeaf5fa9dd85b240200f74b3b2956fbbb
Pablo Fernandez <pupeno@pupeno.com>
2022-11-27 13:20:46 +0000

    Add users. (#44)
    * Add users.
    
    * Add views for users.
    
    * Encapsulate shared views in helper methods.
    
    * Changing password works.
    
    * Add the page title to the header.
    
    * Correct the names of view tests.
    
    * Test users (and add to_s method).
    
    * Added profile page to edit email (and more in the future).
    
    * Hopefully fix CI.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  context "A user" do
+    before { @user = create(:user) }
+
+    it "is readable" do
+      expect(@user.to_s).to include(@user.class.name)
+      expect(@user.to_s).to include(@user.id)
+      expect(@user.to_s).to include(@user.email)
+    end
+  end
+end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :uuid             not null, primary key
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  failed_attempts        :integer          default(0), not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  locked_at              :datetime
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  unconfirmed_email      :string
+#  unlock_token           :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_unlock_token          (unlock_token) UNIQUE
+#
