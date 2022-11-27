c2e68e1ad7f5fa9141cd01fe9c0034c1132cfe7a
Pablo Fernandez <pupeno@pupeno.com>
2022-11-27 12:00:52 +0000

    Encapsulate shared views in helper methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
       "null"
     end
   end
+
+  def badge_checkbox(form, name, label)
+    render partial: "shared/badge_checkbox", locals: {form: form, name: name, label: label}
+  end
+
+  def toggle_checkbox(form, name, check_box_html_options)
+    render partial: "shared/toggle_checkbox", locals: {form: form, name: name, check_box_html_options: check_box_html_options}
+  end
 end
