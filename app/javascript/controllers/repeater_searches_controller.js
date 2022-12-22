2d52a0f0a5e62cfb0b0f55700115af73f8ff1478
Pablo Fernandez <pupeno@pupeno.com>
2022-12-22 20:18:49 +0000

    Mostly working logic for searching and saving searches.

--- app/javascript/controllers/repeater_searches_controller.js
+++ app/javascript/controllers/repeater_searches_controller.js
@@ -1,18 +1,22 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-  saveSearch(event) {
-    // Turn the form into a save form... and let the submit event continue.
-    this.element.setAttribute("method", "post")
-    this.element.setAttribute("action", this.element.getAttribute("data-repeater-searches-url"))
-    const csrfParam = document.head.querySelector("meta[name='csrf-param']").content
-    if (!this.element.querySelector(`input[name=${csrfParam}]`)) {
-      let csrfHidenInput = document.createElement("input")
-      csrfHidenInput.setAttribute("type", "hidden")
-      csrfHidenInput.setAttribute("autocomplete", "off")
-      csrfHidenInput.setAttribute("name", csrfParam)
-      csrfHidenInput.setAttribute("value", document.head.querySelector("meta[name='csrf-token']").content)
-      this.element.appendChild(csrfHidenInput)
+  // Turns the get search form into a post save form.
+  saveSearch() {
+    if (this.element.getAttribute("method") === "get") { // Only act on the get form, not on the patch of the show/edit action.
+      this.element.setAttribute("method", "post")
+      this.element.setAttribute("action", this.element.getAttribute("data-repeater-searches-url"))
+
+      // Add the csrf hidden input if it's not there yet.
+      const csrfParam = document.head.querySelector("meta[name='csrf-param']").content
+      if (!this.element.querySelector(`input[name=${csrfParam}]`)) {
+        let csrfHidenInput = document.createElement("input")
+        csrfHidenInput.setAttribute("type", "hidden")
+        csrfHidenInput.setAttribute("autocomplete", "off")
+        csrfHidenInput.setAttribute("name", csrfParam)
+        csrfHidenInput.setAttribute("value", document.head.querySelector("meta[name='csrf-token']").content)
+        this.element.appendChild(csrfHidenInput)
+      }
     }
   }
 }
