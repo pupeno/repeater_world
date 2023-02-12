a555c83a0fdc9535f50a52304446229b62f324e0
Pablo Fernandez <pablo@pablofernandez.tech>
2023-02-12 15:59:32 +0000

    Readme, license and links. Closes #41 (#105)
    * Improve the readme.
    
    * Links to support, open source and bugs.
    
    * Choose AGPL license.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,16 @@
+# Copyright 2023, Pablo Fernandez
+#
+# This file is part of Repeater World.
+#
+# Repeater World is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License.
+#
+# Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Foobar. If not, see
+# <https://www.gnu.org/licenses/>.
+
 class Admin < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable, :async # :registerable, :omniauthable
 
