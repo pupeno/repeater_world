9cefe16301527ebad7d8a6c204f36764ecacb494
Pablo Fernandez <pablo@pablofernandez.tech>
2023-02-20 00:23:08 +0000

    Allow people to suggest new repeaters. Closes #43 (#115)
    * Move call sign generation out of the repeater factory.
    
    * Make the CSV Exporter test more deterministic.
    
    * Accept suggestions for new repeaters.
    
    * Separate data from view of the data for access method.
    
    * Refactoring: let's call them CTCSS tones, not codes.
    
    * Refactoring: rename DMR fields.
    
    Also, add exporting of redistribution limitations.
    
    * Refactoring: clean up column names in CSV exporter.
    
    * Refactoring: rename region fields.

---
+++ spec/views/suggested_repeaters/new.html.erb_spec.rb
@@ -0,0 +1,28 @@
+# Copyright 2023, Flexpoint Tech
+#
+# This file is part of Repeater World.
+#
+# Repeater World is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License.
+#
+# Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Foobar. If not, see
+# <https://www.gnu.org/licenses/>.
+#
+require "rails_helper"
+
+# TODO: decide if we actually want view tests.
+# RSpec.describe "suggested_repeaters/new", type: :view do
+#   before(:each) do
+#     assign(:suggested_repeater, SuggestedRepeater.new)
+#   end
+#
+#   it "renders new suggested_repeater form" do
+#     render
+#
+#     assert_select "form[action=?][method=?]", suggested_repeaters_path, "post" do
+#     end
+#   end
+# end
