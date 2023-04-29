1b37e6ee160545469494185bbbb24a5aead5e973
Pablo Fernandez <pablo@pablofernandez.tech>
2023-04-29 12:39:16 +0100

    Make the search vs save differences very clear. Closes #126 (#148)
    * Upgrade all the gems.
    
    * Refactoring: clean up auth before save search dialog.
    
    * Add name to repeat searches.
    
    * Tweak the colors of buttons and toggles.
    
    * Initial saving of repeaters with a name (barely working).
    
    * Use field_with_errors.
    
    * Correct workflow for the name field.
    
    * Correct workflow for search and save.
    
    * Remove repeater searches /new, it's not really used.
    Also, add more tests.
    
    * Tidy tests a little bit.
    
    * Add more tests.
    
    * Non-semantic: code formatting.

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   get "search", to: "repeater_searches#new"
   get "export", to: "repeater_searches#export"
 
-  resources :repeater_searches, only: [:new, :create, :show, :update] do
+  resources :repeater_searches, only: [:create, :show, :update] do
     member do
       get "export"
     end
@@ -72,7 +72,6 @@ end
 #                                   export GET    /export(.:format)                                                                                 repeater_searches#export
 #                   export_repeater_search GET    /repeater_searches/:id/export(.:format)                                                           repeater_searches#export
 #                        repeater_searches POST   /repeater_searches(.:format)                                                                      repeater_searches#create
-#                      new_repeater_search GET    /repeater_searches/new(.:format)                                                                  repeater_searches#new
 #                          repeater_search GET    /repeater_searches/:id(.:format)                                                                  repeater_searches#show
 #                                          PATCH  /repeater_searches/:id(.:format)                                                                  repeater_searches#update
 #                                          PUT    /repeater_searches/:id(.:format)                                                                  repeater_searches#update
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_25_104922) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_11_184604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_25_104922) do
     t.decimal "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_repeater_searches_on_user_id"
   end
 
