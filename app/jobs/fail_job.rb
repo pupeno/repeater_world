bb8cb15bc1069155fb74de1d8c96e29538fc7c3e
Pablo Fernandez <pablo@pablofernandez.tech>
2023-05-20 00:35:01 +0100

    Correct the name of the app in the license. Closes #159 (#161)
    * Correct the name of the app in the license.
    
    * Correct the name of the app in the license.
    
    * Correct the name of the app in the license.
    
    * Correct the name of the app in the license.
    
    * Non-semantic: standardrb.

--- app/jobs/fail_job.rb
+++ app/jobs/fail_job.rb
@@ -5,10 +5,11 @@
 # Repeater World is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
 # Public License as published by the Free Software Foundation, either version 3 of the License.
 #
-# Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+# Repeater World is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
 #
-# You should have received a copy of the GNU Affero General Public License along with Foobar. If not, see
+# You should have received a copy of the GNU Affero General Public License along with Repeater World. If not, see
 # <https://www.gnu.org/licenses/>.
 
 class FailJob < ApplicationJob
