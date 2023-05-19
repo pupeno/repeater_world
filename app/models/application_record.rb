edee85028479ccee700e460f2b9b70514cdf3a91
Pablo Fernandez <pablo@pablofernandez.tech>
2023-05-20 00:25:08 +0100

    Correct the name of the app in the license.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -8,7 +8,7 @@
 # Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License along with Foobar. If not, see
+# You should have received a copy of the GNU Affero General Public License along with Repeater World. If not, see
 # <https://www.gnu.org/licenses/>.
 
 class ApplicationRecord < ActiveRecord::Base
