bb6289c6b92f15a3f5b4eb454bef7c946d126912
Pablo Fernandez <pablo@pablofernandez.tech>
2023-05-20 02:13:14 +0100

    Move settings to environment variables.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false # Switch to true only if we are sending a lot of email and we know what we are doing.
   config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = {api_token: Rails.application.credentials.postmark.api_key}
+  config.action_mailer.postmark_settings = {api_token: ENV["POSTMARK_API_TOKEN"]}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
