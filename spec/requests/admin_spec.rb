ed5cf88660fd8af4781e4f3f35c0104cb5b971f3
Pablo Fernandez <pablo@pablofernandez.tech>
2023-05-27 11:57:19 +0100

    Correctly format frequencies everywhere. Closes #204 (#212)
    * Correctly format frequencies everywhere. Closes #204
    
    * Refactoring: move view code out of the model.
    
    * Refactoring: shared view for showing a location.
    
    * Refactoring: shared view for showing a rx frequency.
    
    * Add some tests.

---
+++ spec/requests/admin_spec.rb
@@ -0,0 +1,29 @@
+# Copyright 2023, Pablo Fernandez
+#
+# This file is part of Repeater World.
+#
+# Repeater World is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License.
+#
+# Repeater World is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Repeater World. If not, see
+# <https://www.gnu.org/licenses/>.
+
+require "rails_helper"
+
+RSpec.describe "/admin", type: :request do
+  context "While logged in as an admin" do
+    before do
+      @current_user = create(:admin)
+      sign_in @current_user
+    end
+
+    it "shows the admin dashboard" do
+      get rails_admin_url
+      expect(response).to be_successful # Mostly just checking that it loads.
+    end
+  end
+end
