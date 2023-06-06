41d303acc7a1f72640439464163becbb91226692
Pablo Fernandez <pablo@pablofernandez.tech>
2023-06-06 15:39:26 +0100

    Import data for Australia. Closes #195 (#233)
    * Skeleton for the WIA import.
    
    * Don't try to map repeaters with no location.
    
    * 4 bytes is not enough for frequencies about 10GHz. 🤯
    
    * Finish importing Australia.
    
    * Add tests for Australia import and fix some issues.
    
    * Fix importing power, altitude, status and notes.
    
    * Increase code coverage because I can. 🤷‍♂️

---
+++ spec/lib/importers/importer_spec.rb
@@ -0,0 +1,25 @@
+# Copyright 2023, Pablo Fernandez
+#
+# This file is part of Repeater World.
+#
+# Repeater World is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License.
+#
+# Repeater World is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Repeater World. If not, see
+# <https://www.gnu.org/licenses/>.
+
+require "rails_helper"
+
+RSpec.describe Importer do
+  it "should delegate an interface to subclasses" do
+    expect { Importer.source }.to raise_error(/Importer subclasses must implement this method/)
+    importer = Importer.new
+    expect { importer.import }.to raise_error(/Importer subclasses must implement this method/)
+    importer = Importer.new
+    expect { importer.send(:import_data) }.to raise_error(/Importer subclasses must implement this method/)
+  end
+end
---
+++ spec/lib/importers/wia_importer_spec.rb
@@ -0,0 +1,74 @@
+# Copyright 2023, Pablo Fernandez
+#
+# This file is part of Repeater World.
+#
+# Repeater World is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License.
+#
+# Repeater World is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Repeater World. If not, see
+# <https://www.gnu.org/licenses/>.
+
+require "rails_helper"
+
+RSpec.describe WiaImporter do
+  it "should import" do
+    files = {"https://www.wia.org.au/members/repeaters/data/" => "wia.html",
+             "https://www.wia.org.au/members/repeaters/data/documents/Repeater%20Directory%20230304.csv" => "wia.csv"}
+
+    files.each do |url, local_file|
+      file = double("file")
+      local_file = Rails.root.join("spec", "factories", "wia_importer_data", local_file)
+      expect(file).to receive(:open).and_return(File.new(local_file))
+      expect(URI).to receive(:parse).with(url).and_return(file)
+    end
+
+    Dir.mktmpdir("WiaImporter") do |dir|
+      expect do
+        WiaImporter.new(working_directory: dir).import
+      end.to change { Repeater.count }.by(731)
+
+      expect(Repeater.where(call_sign: "VK7REC").count).to eq(2)
+
+      repeater = Repeater.find_sole_by(call_sign: "VK4RSP")
+      expect(repeater.name).to eq("Roddas Lookout VK4RSP")
+      expect(repeater.band).to eq(Repeater::BAND_2M)
+      expect(repeater.tx_frequency).to eq(146_825_000)
+      expect(repeater.rx_frequency).to eq(146_225_000)
+
+      # The second time we call it, it shouldn't re-download any files, nor create new
+      # repeaters
+      expect do
+        WiaImporter.new(working_directory: dir).import
+      end.to change { Repeater.count }.by(0)
+
+      # Some repeaters change, some get disconnected from the source, other's don't.
+      repeater = Repeater.find_by(call_sign: "VK1RAC")
+      repeater.rx_frequency = 1_000_000
+      repeater.source = nil
+      repeater.redistribution_limitations = nil
+      repeater.save!
+      repeater = Repeater.find_by(call_sign: "VK1RBH")
+      repeater.rx_frequency = 1_000_000
+      repeater.save!
+      create(:repeater, :full, call_sign: "VK2RAG", tx_frequency: 145_000_001, source: WiaImporter.source)
+
+      # The third time we call it, it shouldn't re-download any files, nor create new
+      # repeaters, but some get updated, some don't, and some get deleted.
+      expect(Repeater.where(call_sign: "VK2RAG").count).to eq(9)
+      expect do
+        WiaImporter.new(working_directory: dir).import
+      end.to change { Repeater.count }.by(-1)
+      expect(Repeater.where(call_sign: "VK2RAG").count).to eq(8)
+      repeater = Repeater.find_by(call_sign: "VK1RAC") # This one didn't change.
+      expect(repeater.rx_frequency).to eq(1_000_000)
+      repeater = Repeater.find_by(call_sign: "VK1RBH") # This one did
+      expect(repeater.rx_frequency).to eq(147_775_000)
+      repeater = Repeater.find_by(call_sign: "VK2RAG", tx_frequency: 145_000_001) # This one got deleted
+      expect(repeater).to be(nil)
+    end
+  end
+end
