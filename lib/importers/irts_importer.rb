8054f36f64cd0a8ec24ecdcf941506af7b00b416
Pablo Fernandez <pablo@pablofernandez.tech>
2023-06-16 15:10:46 +0100

    Importer for IRTS (Ireland). Closes #197 (#237)
    * Fix the name of the wia importer.
    
    * IRTS importer working.
    
    * Added tests for IRTS importer, and fixed a couple of things.
    
    * Fixed linting issues.

---
+++ lib/importers/irts_importer.rb
@@ -0,0 +1,162 @@
+# Copyright 2023, Pablo Fernandez
+#
+# This file is part of Repeater World.
+#
+# Repeater World is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License.
+#
+# Repeater World is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Repeater World. If not, see
+# <https://www.gnu.org/licenses/>.
+
+class IrtsImporter < Importer
+  def self.source
+    "https://www.irts.ie/cgi/repeater.cgi"
+  end
+
+  private
+
+  EXPORT_URL = "https://www.irts.ie/cgi/repeater.cgi?printable"
+
+  def import_data
+    ignored_due_to_source_count = 0
+    created_or_updated_ids = []
+    repeaters_deleted_count = 0
+
+    file_name = download_file(EXPORT_URL, "irts.html")
+
+    doc = Nokogiri::HTML(File.read(file_name))
+    table = doc.at("table")
+
+    Repeater.transaction do
+      table.search("tr").each do |row|
+        row = row.search("td")
+        next if row.empty?
+
+        action, imported_repeater = import_repeater(row)
+        if action == :ignored_due_to_source
+          ignored_due_to_source_count += 1
+        elsif action == :ignored_due_to_broken_record
+          # Nothing to do really. Should we track this?
+        else
+          created_or_updated_ids << imported_repeater.id
+        end
+      rescue
+        raise "Failed to import record #{row}"
+      end
+
+      repeaters_deleted_count = Repeater.where(source: self.class.source).where.not(id: created_or_updated_ids).delete_all
+    end
+
+    [ignored_due_to_source_count, created_or_updated_ids, repeaters_deleted_count]
+  end
+
+  CHANNEL = 0
+  FREQUENCY = 1
+  CALL_SIGN = 2
+  ACCESS = 3
+  LOCATION = 4
+  NOTES = 5
+
+  FREQUENCY_REGEX = /Output:[^\d]+([\d.]+)Input:[^\d]+([\d.]+)/
+
+  def import_repeater(row)
+    call_sign = row[CALL_SIGN].text.strip.upcase
+    tx_frequency = row[FREQUENCY].text.scan(FREQUENCY_REGEX).flatten.first.to_f * 10**6
+
+    repeater = Repeater.find_or_initialize_by(call_sign: call_sign, tx_frequency: tx_frequency)
+
+    # Only update repeaters that were sourced from this same source.
+    if repeater.persisted? && repeater.source != self.class.source
+      @logger.info "Not updating #{repeater} since the source is #{repeater.source.inspect} and not #{self.class.source.inspect}"
+      return [:ignored_due_to_source, repeater]
+    end
+    repeater.name = repeater.call_sign
+
+    repeater.channel = row[CHANNEL].text.strip
+    repeater.rx_frequency = row[FREQUENCY].text.scan(FREQUENCY_REGEX).flatten.second.to_f * 10**6
+    import_mode_access_code(repeater, row[ACCESS].text.strip, row[NOTES].text.strip)
+    import_location(repeater, row[LOCATION])
+    repeater.notes = row[NOTES].text.strip
+
+    repeater.country_id = "ie"
+    repeater.source = self.class.source
+
+    repeater.save!
+    [:created_or_updated, repeater]
+  end
+
+  def import_mode_access_code(repeater, access, notes)
+    access_codes = access.split("/")
+    access_codes.each do |access_code|
+      if access_code.to_d == BigDecimal("1750.0")
+        repeater.fm = true
+        repeater.fm_tone_burst = true
+      elsif access_code.to_f.in? Repeater::CTCSS_TONES
+        repeater.fm = true
+        repeater.fm_ctcss_tone = access_code.to_f
+      elsif access_code.to_d == 110 # Assuming it's a typo and should be 110.9.
+        repeater.fm = true
+        repeater.fm_ctcss_tone = 110.9
+      elsif access_code.strip == "Wires X"
+        repeater.fusion = true # Assuming Wires X means System Fusion.
+      elsif access_code.strip == "C4FM"
+        repeater.fusion = true
+      elsif access_code.strip == "Carrier"
+        repeater.fm = true # What is Carrier? Just pressing the PTT with no tone at all?
+      elsif access_code.strip == "MMDVM"
+        # This seems to just indicate that it's multi mode, but not how to access.
+      elsif access_code.strip == "DMR CC1"
+        repeater.dmr = true
+        repeater.dmr_color_code = 1
+      elsif access_code.strip == "DMR CC2"
+        repeater.dmr = true
+        repeater.dmr_color_code = 2
+      elsif access_code.strip == "Dstar"
+        repeater.dstar = true
+      else
+        raise "Unknown access code #{access_code}"
+      end
+    end
+    if "FM".in? notes
+      repeater.fm = true
+    end
+    if "DStar".in? notes
+      repeater.dstar = true
+    end
+    if "Fusion".in?(notes) || "C4FM".in?(notes)
+      repeater.fusion = true
+    end
+    if "DMR".in? notes
+      repeater.dmr = true
+    end
+    if "NXDN".in? notes
+      repeater.nxdn = true
+    end
+  end
+
+  def import_location(repeater, location)
+    location = location.xpath("text()").map(&:text)
+
+    repeater.grid_square = location.last
+
+    address = location[0..-2].join(",")
+    address = address.split(",").map { |x| x.strip.gsub(/\s+/, " ").tr("\u00A0", " ") }
+
+    if address.size == 1
+      if address[0].start_with? "Co"
+        repeater.locality = nil
+        repeater.region = address.first
+      else
+        repeater.locality = address.first
+        repeater.region = nil
+      end
+    else
+      repeater.locality = address[0..-2].join(",")
+      repeater.region = address[-1]
+    end
+  end
+end
---
+++ spec/lib/importers/irts_importer_spec.rb
@@ -0,0 +1,73 @@
+# Copyright 2023, Pablo Fernandez
+#
+# This file is part of Repeater World.
+#
+# Repeater World is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License.
+#
+# Repeater World is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Repeater World. If not, see
+# <https://www.gnu.org/licenses/>.
+
+require "rails_helper"
+
+RSpec.describe IrtsImporter do
+  it "should import" do
+    files = {"https://www.irts.ie/cgi/repeater.cgi?printable" => "irts.html"}
+
+    files.each do |url, local_file|
+      file = double("file")
+      local_file = Rails.root.join("spec", "factories", "irts_importer_data", local_file)
+      expect(file).to receive(:open).and_return(File.new(local_file))
+      expect(URI).to receive(:parse).with(url).and_return(file)
+    end
+
+    Dir.mktmpdir("IrtsImporter") do |dir|
+      expect do
+        IrtsImporter.new(working_directory: dir).import
+      end.to change { Repeater.count }.by(71)
+
+      expect(Repeater.where(call_sign: "EI0IPG").count).to eq(1)
+
+      repeater = Repeater.find_sole_by(call_sign: "EI0IPG")
+      expect(repeater.name).to eq("EI0IPG")
+      expect(repeater.band).to eq(Repeater::BAND_10M)
+      expect(repeater.tx_frequency).to eq(29_680_000)
+      expect(repeater.rx_frequency).to eq(29_580_000)
+
+      # The second time we call it, it shouldn't re-download any files, nor create new
+      # repeaters
+      expect do
+        IrtsImporter.new(working_directory: dir).import
+      end.to change { Repeater.count }.by(0)
+
+      # Some repeaters change, some get disconnected from the source, other's don't.
+      repeater = Repeater.find_by(call_sign: "EI4SMR")
+      repeater.rx_frequency = 1_000_000
+      repeater.source = nil
+      repeater.redistribution_limitations = nil
+      repeater.save!
+      repeater = Repeater.find_by(call_sign: "EI4SNR")
+      repeater.rx_frequency = 1_000_000
+      repeater.save!
+      create(:repeater, :full, call_sign: "EI2TRR", tx_frequency: 145_000_001, source: IrtsImporter.source)
+
+      # The third time we call it, it shouldn't re-download any files, nor create new
+      # repeaters, but some get updated, some don't, and some get deleted.
+      expect(Repeater.where(call_sign: "EI2TRR").count).to eq(2)
+      expect do
+        IrtsImporter.new(working_directory: dir).import
+      end.to change { Repeater.count }.by(-1)
+      expect(Repeater.where(call_sign: "EI2TRR").count).to eq(1)
+      repeater = Repeater.find_by(call_sign: "EI4SMR") # This one didn't change.
+      expect(repeater.rx_frequency).to eq(1_000_000)
+      repeater = Repeater.find_by(call_sign: "EI4SNR") # This one did
+      expect(repeater.rx_frequency).to eq(70_475_000)
+      repeater = Repeater.find_by(call_sign: "EI2TRR", tx_frequency: 145_000_001) # This one got deleted
+      expect(repeater).to be(nil)
+    end
+  end
+end
