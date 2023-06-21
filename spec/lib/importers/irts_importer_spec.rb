d1e9141f75b691cbbe464043567a4c213d220734
Pablo Fernandez <pablo@pablofernandez.tech>
2023-06-21 05:09:58 +0100

    Much more readable importer test.

--- spec/lib/importers/irts_importer_spec.rb
+++ spec/lib/importers/irts_importer_spec.rb
@@ -15,59 +15,83 @@
 require "rails_helper"
 
 RSpec.describe IrtsImporter do
-  it "should import" do
+  before do
+    Repeater.delete_all
     files = {"https://www.irts.ie/cgi/repeater.cgi?printable" => "irts.html"}
-
     files.each do |url, local_file|
       file = double("file")
       local_file = Rails.root.join("spec", "factories", "irts_importer_data", local_file)
       expect(file).to receive(:open).and_return(File.new(local_file))
       expect(URI).to receive(:parse).with(url).and_return(file)
     end
+  end
 
+  it "should import" do
     Dir.mktmpdir("IrtsImporter") do |dir|
       expect do
         IrtsImporter.new(working_directory: dir).import
       end.to change { Repeater.count }.by(44)
 
-      expect(Repeater.where(call_sign: "EI0IPG").count).to eq(1)
-
+      # Grab one repeater and verify it was imported correctly.
       repeater = Repeater.find_sole_by(call_sign: "EI0IPG")
       expect(repeater.name).to eq("EI0IPG")
       expect(repeater.band).to eq(Repeater::BAND_10M)
       expect(repeater.tx_frequency).to eq(29_680_000)
       expect(repeater.rx_frequency).to eq(29_580_000)
+    end
+  end
 
-      # The second time we call it, it shouldn't re-download any files, nor create new
-      # repeaters
+  it "should not import anything new on a second pass" do
+    Dir.mktmpdir("IrtsImporter") do |dir|
+      IrtsImporter.new(working_directory: dir).import
+
+      # It should not import anything new on a second pass and also it shouldn't re-download files.
       expect do
         IrtsImporter.new(working_directory: dir).import
       end.to change { Repeater.count }.by(0)
+    end
+  end
 
-      # Some repeaters change, some get disconnected from the source, other's don't.
-      repeater = Repeater.find_by(call_sign: "EI4SMR")
+  it "should respect the source values during import" do
+    Dir.mktmpdir("IrtsImporter") do |dir|
+      IrtsImporter.new(working_directory: dir).import
+
+      # This repeater simulates a previously imported repeater that is no longer in the source files, so we should
+      # delete it to avoid stale data.
+      will_delete = "EI2TRR"
+      create(:repeater, :full, call_sign: will_delete, tx_frequency: 145_000_001, source: IrtsImporter.source)
+
+      # This repeater represents one that got taken over by the owner becoming a Repeater World user, that means the
+      # source is now nil. This should never again be overwritten by the importer.
+      wont_update = "EI4SMR"
+      repeater = Repeater.find_by(call_sign: wont_update)
       repeater.rx_frequency = 1_000_000
       repeater.source = nil
-      repeater.redistribution_limitations = nil
       repeater.save!
-      repeater = Repeater.find_by(call_sign: "EI4SNR")
+
+      # This repeater represents one where the upstream data changed and should be updated by the importer.
+      will_update = "EI4SNR"
+      repeater = Repeater.find_by(call_sign: will_update)
       repeater.rx_frequency = 1_000_000
       repeater.save!
-      create(:repeater, :full, call_sign: "EI2TRR", tx_frequency: 145_000_001, source: IrtsImporter.source)
 
-      # The third time we call it, it shouldn't re-download any files, nor create new
-      # repeaters, but some get updated, some don't, and some get deleted.
-      expect(Repeater.where(call_sign: "EI2TRR").count).to eq(2)
+      # Run the import and verify we removed one repeater but otherwise made no changes.
       expect do
         IrtsImporter.new(working_directory: dir).import
       end.to change { Repeater.count }.by(-1)
-      expect(Repeater.where(call_sign: "EI2TRR").count).to eq(1)
-      repeater = Repeater.find_by(call_sign: "EI4SMR") # This one didn't change.
-      expect(repeater.rx_frequency).to eq(1_000_000)
-      repeater = Repeater.find_by(call_sign: "EI4SNR") # This one did
-      expect(repeater.rx_frequency).to eq(70_475_000)
-      repeater = Repeater.find_by(call_sign: "EI2TRR", tx_frequency: 145_000_001) # This one got deleted
+        .and change { Repeater.where(call_sign: will_delete, tx_frequency: 145_000_001).count }.by(-1)
+
+      # This one got deleted
+      repeater = Repeater.find_by(call_sign: will_delete, tx_frequency: 145_000_001)
       expect(repeater).to be(nil)
+
+      # This one didn't change.
+      repeater = Repeater.find_by(call_sign: wont_update)
+      expect(repeater.rx_frequency).to eq(1_000_000)
+
+      # This got updated.
+      repeater = Repeater.find_by(call_sign: will_update)
+      expect(repeater.rx_frequency).to eq(70_475_000)
     end
   end
 end
