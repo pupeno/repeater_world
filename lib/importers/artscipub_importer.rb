5736cdf5ce96d8bba9309f565527d15d09c74390
Pablo Fernandez <pablo@pablofernandez.tech>
2023-06-23 14:51:05 +0100

    Retrieve all files and parse them into hashes.

--- lib/importers/artscipub_importer.rb
+++ lib/importers/artscipub_importer.rb
@@ -27,27 +27,63 @@ class ArtscipubImporter < Importer
     repeaters_deleted_count = 0
 
     home_file_name = download_file(EXPORT_URL, "home.html")
-    home = Nokogiri::HTML(File.read(home_file_name))
+    home = Nokogiri::HTML(File.read(home_file_name), nil, "windows-1252")
+
+    raw_repeaters = []
 
     home.search("a[href^=\"/repeaters/search/index.asp?state=\"]").each do |state_country_link|
       page = 1
       state_country_name = state_country_link.text.gsub(/^\./, "_")
       current_url = EXPORT_URL
-      begin
+
+      loop do
         current_url = URI.join(current_url, state_country_link["href"]).to_s
         list_file_name = download_file(current_url, "#{state_country_name}_#{page}.html")
-        puts list_file_name
-        list = Nokogiri::HTML(File.read(list_file_name))
-        state_country_link = list.at("a[text()=' Next ']")
-        puts "state_country_link.inspect: #{state_country_link.inspect}"
+        list_file = Nokogiri::HTML(File.read(list_file_name), nil, "windows-1252")
 
+        list_file.search("a[href^=\"/repeaters/detail.asp?rid=\"]").each do |repeater_link|
+          repeater_id = repeater_link["href"].match(/rid=(\d+)&/)[1]
+          repeater_file_name = download_file(URI.join(current_url, URI::DEFAULT_PARSER.escape(repeater_link["href"])).to_s, "#{repeater_id}.html")
+          repeater_file = Nokogiri::HTML(File.read(repeater_file_name), nil, "windows-1252")
+          raw_repeaters << extract_raw_repeater(repeater_file)
+        end
+
+        # Next!
+        state_country_link = list_file.at("a[text()=' Next ']")
         page += 1
-      end while state_country_link.present?
+        break if state_country_link.blank?
+      end
     end
 
     # puts @mocks
 
+    # TODO: replace Ø with 0.
+
     [ignored_due_to_source_count, created_or_updated_ids, repeaters_deleted_count]
+  end
+
+  def extract_raw_repeater(repeater_file)
+    raw_repeater = {}
+
+    # First find the table with the data.
+    table = repeater_file.at_xpath("//td[normalize-space() = 'Repeater ID #']/ancestor::*[name()='table'][position() = 1]")
+
+    raw_repeater[:external_id] = table.at_xpath("tr[td[normalize-space() = 'Repeater ID #']]/td[2]").text.match(/(\d+)/)[1] # There's other text in the external_id cell that we don't want.
+    raw_repeater[:call_sign] = table.at_xpath("tr[td[normalize-space() = 'Call Sign']]/td[2]").text&.strip
+    raw_repeater[:location] = table.at_xpath("tr[td[normalize-space() = 'Location']]/td[2]").text&.strip
+    raw_repeater[:frequency] = table.at_xpath("tr[td[normalize-space() = 'Frequency']]/td[2]").text&.strip
+    raw_repeater[:input] = table.at_xpath("tr[td[normalize-space() = 'Input']]/td[2]").text&.strip
+    raw_repeater[:pl_tone] = table.at_xpath("tr[td[normalize-space() = 'PL Tone']]/td[2]").text&.strip
+    raw_repeater[:rating] = table.at_xpath("tr[td[normalize-space() = 'Rating']]/td[2]").text&.strip
+    raw_repeater[:auto_patch] = table.at_xpath("tr[td[normalize-space() = 'Auto Patch']]/td[2]").text&.strip
+    raw_repeater[:web_site] = table.at_xpath("tr[td[normalize-space() = 'Web Site']]/td[2]").text&.strip
+    raw_repeater[:grid_square] = table.at_xpath("tr[td[normalize-space() = 'Grid Square']]/td[2]").text&.strip
+    raw_repeater[:zip_code] = table.at_xpath("tr[td[normalize-space() = 'Zip Code.']]/td[2]//b").text&.strip # This one is different, because there's some cruft in this cell.
+    raw_repeater[:latitude] = table.at_xpath("tr[td[normalize-space() = 'Latitude']]/td[2]").text&.strip
+    raw_repeater[:longitude] = table.at_xpath("tr[td[normalize-space() = 'Longitude']]/td[2]").text&.strip
+    raw_repeater[:comments] = table.at_xpath("tr[td[normalize-space() = 'Comments']]/td[2]").text&.strip
+
+    raw_repeater
   end
 
   def download_file(url, dest)
