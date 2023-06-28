c782e6f3a278899568d9d2e172c541240ed0e333
Pablo Fernandez <pablo@pablofernandez.tech>
2023-06-28 17:09:40 +0100

    Report to Sentry instead of printing to the console.

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -14,6 +14,7 @@
  */
 
 import {Controller} from "@hotwired/stimulus"
+import * as Sentry from "@sentry/browser"
 import {MarkerClusterer} from "@googlemaps/markerclusterer"
 
 export default class extends Controller {
@@ -34,7 +35,7 @@ export default class extends Controller {
 
     const markers = this.markersValue.map(marker => {
       if(typeof marker.lat !== "number" || typeof marker.lng !== "number") {
-        console.log("Invalid marker: ", marker)
+        Sentry.captureMessage(`Invalid marker: ${marker}`)
         return null
       }
 
