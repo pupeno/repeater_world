83027eb1ecc81f39b77ea48274098619b7c62c33
Pablo Fernandez <pablo@pablofernandez.tech>
2023-07-01 10:30:46 +0100

    Apply new hover and focus styles to the top bar.

--- app/helpers/top_bar_helper.rb
+++ app/helpers/top_bar_helper.rb
@@ -15,59 +15,79 @@
 module TopBarHelper
   def top_bar_desktop_link(*args, **kwargs, &block)
     kwargs[:class] = <<-CLASSES
-      inline-flex items-center px-1 pt-1 border-b-2 font-medium 
-      focus:ring-inset focus:outline-none focus:ring-2 focus:ring-orange-500
+      inline-flex items-center px-1 pt-1 border-b-4 font-medium
+      focus:outline-dotted focus:bg-orange-50 outline-2 outline-offset-[-2px] outline-orange-600
+      dark:focus:bg-gray-700
     CLASSES
     kwargs[:class_inactive] = <<-CLASSES
-        border-transparent text-gray-500 hover:text-gray-800 focus:text-gray-800 hover:border-gray-800 focus:border-gray-800
-        dark:text-gray-400 dark:hover:text-gray-200 dark:focus:text-gray-200 dark:hover:border-gray-200 dark:focus:border-gray-200
+        text-gray-500
+        dark:text-gray-400 
+        border-transparent  
+        hover:text-gray-800 hover:border-gray-800
+        dark:hover:text-gray-200 dark:hover:border-gray-200
+        focus:text-gray-800 focus:border-gray-800
+        dark:focus:text-gray-200 dark:focus:border-gray-200
     CLASSES
     kwargs[:class_active] = "border-orange-500 text-gray-900 dark:text-gray-100"
     kwargs[:role] = "menuitem"
     active_link_to(*args, **kwargs, &block)
   end
 
-  TOP_BAR_DESKTOP_EXTRA_BASE_CLASS = "block px-6 py-4 border-l-4 font-medium dark:text-gray-100"
-  TOP_BAR_DESKTOP_EXTRA_INACTIVE_CLASS = <<-CLASSES
-    border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 
-    dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:border-gray-500 dark:hover:text-gray-400"
+  DESKTOP_EXTRA_BASE_CLASS = <<-CLASSES
+    block px-6 py-4 border-l-4 font-medium dark:text-gray-100
+    focus:outline-dotted outline-2 outline-offset-[-2px] outline-orange-600
   CLASSES
-  TOP_BAR_DESKTOP_EXTRA_ACTIVE_CLASS = <<-CLASSES
+  DESKTOP_EXTRA_INACTIVE_CLASS = <<-CLASSES
+    border-transparent text-gray-600
+    dark:text-gray-300 
+    hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 
+    dark:hover:bg-gray-600 dark:hover:border-gray-300 dark:hover:text-gray-400
+    focus:bg-gray-50 focus:border-gray-300 focus:text-gray-800 
+    dark:focus:bg-gray-600 dark:focus:border-gray-300 dark:focus:text-gray-400"
+  CLASSES
+  DESKTOP_EXTRA_ACTIVE_CLASS = <<-CLASSES
     bg-orange-50 border-orange-500 text-orange-700
     dark:bg-orange-950 dark:text-orange-300
   CLASSES
 
   def top_bar_desktop_extra_link(*args, **kwargs, &block)
-    kwargs[:class] = TOP_BAR_DESKTOP_EXTRA_BASE_CLASS
-    kwargs[:class_inactive] = TOP_BAR_DESKTOP_EXTRA_INACTIVE_CLASS
-    kwargs[:class_active] = TOP_BAR_DESKTOP_EXTRA_ACTIVE_CLASS
+    kwargs[:class] = DESKTOP_EXTRA_BASE_CLASS
+    kwargs[:class_inactive] = DESKTOP_EXTRA_INACTIVE_CLASS
+    kwargs[:class_active] = DESKTOP_EXTRA_ACTIVE_CLASS
     kwargs[:role] = "menuitem"
     active_link_to(*args, **kwargs, &block)
   end
 
   def top_bar_desktop_extra_button_class
-    "w-full text-left #{TOP_BAR_DESKTOP_EXTRA_BASE_CLASS} #{TOP_BAR_DESKTOP_EXTRA_INACTIVE_CLASS}"
+    "w-full text-left #{DESKTOP_EXTRA_BASE_CLASS} #{DESKTOP_EXTRA_INACTIVE_CLASS}"
   end
 
-  TOP_BAR_MOBILE_BASE_CLASS = "block pl-3 pr-4 py-4 border-l-4 font-medium"
-  TOP_BAR_MOBILE_INACTIVE_CLASS = <<-CLASSES
-    border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 
-    dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:border-gray-500 dark:hover:text-gray-400"
+  MOBILE_BASE_CLASS = <<-CLASSES
+    block pl-3 pr-4 py-4 border-l-4 font-medium
+    focus:outline-dotted outline-2 outline-offset-[-2px] outline-orange-600
   CLASSES
-  TOP_BAR_MOBILE_ACTIVE_CLASS = <<-CLASSES
+  MOBILE_INACTIVE_CLASS = <<-CLASSES
+    border-transparent text-gray-600
+    dark:text-gray-300 
+    hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 
+    dark:hover:bg-gray-600 dark:hover:border-gray-300 dark:hover:text-gray-400
+    focus:bg-gray-50 focus:border-gray-300 focus:text-gray-800 
+    dark:focus:bg-gray-600 dark:focus:border-gray-300 dark:focus:text-gray-400"
+  CLASSES
+  MOBILE_ACTIVE_CLASS = <<-CLASSES
     bg-orange-50 border-orange-500 text-orange-700
     dark:bg-orange-950 dark:text-orange-300
   CLASSES
 
   def top_bar_mobile_link(*args, **kwargs, &block)
-    kwargs[:class] = TOP_BAR_MOBILE_BASE_CLASS
-    kwargs[:class_inactive] = TOP_BAR_MOBILE_INACTIVE_CLASS
-    kwargs[:class_active] = TOP_BAR_MOBILE_ACTIVE_CLASS
+    kwargs[:class] = MOBILE_BASE_CLASS
+    kwargs[:class_inactive] = MOBILE_INACTIVE_CLASS
+    kwargs[:class_active] = MOBILE_ACTIVE_CLASS
     kwargs[:role] = "menuitem"
     active_link_to(*args, **kwargs, &block)
   end
 
   def top_bar_mobile_button_class
-    "w-full text-left #{TOP_BAR_MOBILE_BASE_CLASS} #{TOP_BAR_MOBILE_INACTIVE_CLASS}"
+    "w-full text-left #{MOBILE_BASE_CLASS} #{MOBILE_INACTIVE_CLASS}"
   end
 end
