d8329410117abcc57705b12d908103f29b1c3236
Pablo Fernandez <pablo@pablofernandez.tech>
2023-07-04 08:49:23 +0100

    Upgrade all JavaScript libraries.

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,25 +3,24 @@
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "application", preload: true
 
-pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.1.1/js/fontawesome.js"
-pin "@fortawesome/fontawesome-svg-core", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-svg-core@6.1.1/index.es.js"
-pin "@fortawesome/free-brands-svg-icons", to: "https://ga.jspm.io/npm:@fortawesome/free-brands-svg-icons@6.1.1/index.es.js"
-pin "@fortawesome/free-regular-svg-icons", to: "https://ga.jspm.io/npm:@fortawesome/free-regular-svg-icons@6.1.1/index.es.js"
-pin "@fortawesome/free-solid-svg-icons", to: "https://ga.jspm.io/npm:@fortawesome/free-solid-svg-icons@6.1.1/index.es.js"
-pin "@googlemaps/markerclusterer", to: "https://ga.jspm.io/npm:@googlemaps/markerclusterer@2.0.15/dist/index.esm.js"
+pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.4.0/js/fontawesome.js"
+pin "@fortawesome/fontawesome-svg-core", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-svg-core@6.4.0/index.mjs"
+pin "@fortawesome/free-brands-svg-icons", to: "https://ga.jspm.io/npm:@fortawesome/free-brands-svg-icons@6.4.0/index.mjs"
+pin "@fortawesome/free-regular-svg-icons", to: "https://ga.jspm.io/npm:@fortawesome/free-regular-svg-icons@6.4.0/index.mjs"
+pin "@fortawesome/free-solid-svg-icons", to: "https://ga.jspm.io/npm:@fortawesome/free-solid-svg-icons@6.4.0/index.mjs"
+pin "@googlemaps/markerclusterer", to: "https://ga.jspm.io/npm:@googlemaps/markerclusterer@2.3.1/dist/index.esm.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@sentry/browser", to: "https://ga.jspm.io/npm:@sentry/browser@6.19.2/esm/index.js"
-pin "@sentry/core", to: "https://ga.jspm.io/npm:@sentry/core@6.19.2/esm/index.js"
-pin "@sentry/hub", to: "https://ga.jspm.io/npm:@sentry/hub@6.19.2/esm/index.js"
-pin "@sentry/minimal", to: "https://ga.jspm.io/npm:@sentry/minimal@6.19.2/esm/index.js"
-pin "@sentry/tracing", to: "https://ga.jspm.io/npm:@sentry/tracing@6.19.2/esm/index.js"
-pin "@sentry/types", to: "https://ga.jspm.io/npm:@sentry/types@6.19.2/esm/index.js"
-pin "@sentry/utils", to: "https://ga.jspm.io/npm:@sentry/utils@6.19.2/esm/index.js"
+pin "@sentry-internal/tracing", to: "https://ga.jspm.io/npm:@sentry-internal/tracing@7.57.0/esm/index.js"
+pin "@sentry/browser", to: "https://ga.jspm.io/npm:@sentry/browser@7.57.0/esm/index.js"
+pin "@sentry/core", to: "https://ga.jspm.io/npm:@sentry/core@7.57.0/esm/index.js"
+pin "@sentry/replay", to: "https://ga.jspm.io/npm:@sentry/replay@7.57.0/esm/index.js"
+pin "@sentry/utils", to: "https://ga.jspm.io/npm:@sentry/utils@7.57.0/esm/index.js"
+pin "@sentry/utils/esm/buildPolyfills", to: "https://ga.jspm.io/npm:@sentry/utils@7.57.0/esm/buildPolyfills/index.js"
 pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js"
-pin "fast-deep-equal", to: "https://ga.jspm.io/npm:fast-deep-equal@3.1.3/index.js" # Needed by @googlemaps/markerclusterer
-pin "js-cookie", to: "https://ga.jspm.io/npm:js-cookie@3.0.1/dist/js.cookie.mjs"
-pin "kdbush", to: "https://ga.jspm.io/npm:kdbush@3.0.0/kdbush.js" # Needed by @googlemaps/markerclusterer
-pin "supercluster", to: "https://ga.jspm.io/npm:supercluster@7.1.5/index.js" # Needed by @googlemaps/markerclusterer
-pin "tslib", to: "https://ga.jspm.io/npm:tslib@1.14.1/tslib.es6.js"
+pin "fast-deep-equal", to: "https://ga.jspm.io/npm:fast-deep-equal@3.1.3/index.js"
+pin "js-cookie", to: "https://ga.jspm.io/npm:js-cookie@3.0.5/dist/js.cookie.mjs"
+pin "kdbush", to: "https://ga.jspm.io/npm:kdbush@4.0.2/index.js"
+pin "supercluster", to: "https://ga.jspm.io/npm:supercluster@8.0.1/index.js"
+pin "tslib", to: "https://ga.jspm.io/npm:tslib@2.6.0/tslib.es6.mjs"
