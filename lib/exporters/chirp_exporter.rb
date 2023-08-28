091f8770a40a13cb649fd05ae62d0d1930cf3228
Pablo Fernandez <pablo@pablofernandez.tech>
2023-08-28 10:25:49 +0100

    Add separate CHIRP exporter from Baofeng UV-5R

---
+++ lib/exporters/chirp_exporter.rb
@@ -0,0 +1,80 @@
+# Copyright 2023, Flexpoint Tech
+#
+# This file is part of Repeater World.
+#
+# Repeater World is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License.
+#
+# Repeater World is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Repeater World. If not, see
+# <https://www.gnu.org/licenses/>.
+
+class ChirpExporter < Exporter
+  def export
+    headers = [
+      LOCATION, NAME, FREQUENCY, DUPLEX, OFFSET, TONE, R_TONE_FREQ, C_TONE_FREQ, DTCS_CODE, DTCS_POLARITY, MODE, TSTEP,
+      SKIP, COMMENT, URCALL, RPT1CALL, RPT2CALL, DVCODE
+    ]
+
+    @repeaters = @repeaters.where(band: [Repeater::BAND_2M, Repeater::BAND_70CM])
+      .where("operational IS NOT FALSE") # Skip repeaters known to not be operational.
+      .where(fm: true)
+      .order(:name, :call_sign)
+
+    CSV.generate(headers: headers, write_headers: true) do |csv|
+      @repeaters.each_with_index do |repeater, index|
+        csv << to_repeater_row(repeater).merge({LOCATION => index})
+      end
+    end
+  end
+
+  private
+
+  LOCATION = "Location"
+  NAME = "Name"
+  FREQUENCY = "Frequency"
+  DUPLEX = "Duplex"
+  OFFSET = "Offset"
+  TONE = "Tone"
+  R_TONE_FREQ = "rToneFreq"
+  C_TONE_FREQ = "cToneFreq"
+  DTCS_CODE = "DtcsCode"
+  DTCS_POLARITY = "DtcsPolarity"
+  MODE = "Mode"
+  TSTEP = "TStep"
+  SKIP = "Skip"
+  COMMENT = "Comment"
+  URCALL = "URCALL"
+  RPT1CALL = "RPT1CALL"
+  RPT2CALL = "RPT2CALL"
+  DVCODE = "DVCODE"
+
+  def to_repeater_row(repeater)
+    row = {
+      FREQUENCY => frequency_in_mhz(repeater.rx_frequency, precision: 6),
+      DUPLEX => (repeater.tx_frequency > repeater.rx_frequency) ? "-" : "+",
+      OFFSET => frequency_in_mhz((repeater.tx_frequency - repeater.rx_frequency).abs, precision: 6),
+      DTCS_CODE => "023", # TODO: what's this? https://github.com/flexpointtech/repeater_world/issues/28
+      DTCS_POLARITY => "NN", # TODO: what's this? https://github.com/flexpointtech/repeater_world/issues/28
+      MODE => "FM", # Always FM.
+      TSTEP => 5, # Default?
+      COMMENT => "#{repeater.name} #{repeater.call_sign}"
+    }
+
+    row[NAME] =
+      truncate(7, repeater.call_sign)
+
+    row[TONE] = if repeater.fm_ctcss_tone.present?
+      "TONE" # TODO: when do we use TSQL: https://github.com/flexpointtech/repeater_world/issues/23
+    end
+
+    row[R_TONE_FREQ] = repeater.fm_ctcss_tone || 88.5 # Default?
+
+    row[C_TONE_FREQ] = row[R_TONE_FREQ]
+
+    row
+  end
+end
---
+++ spec/lib/exporters/chirp_exporter_spec.rb
@@ -0,0 +1,33 @@
+# Copyright 2023, Flexpoint Tech
+#
+# This file is part of Repeater World.
+#
+# Repeater World is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License.
+#
+# Repeater World is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Repeater World. If not, see
+# <https://www.gnu.org/licenses/>.
+
+require "rails_helper"
+
+RSpec.describe ChirpExporter do
+  include_context "repeaters"
+
+  it "should export" do
+    exporter = ChirpExporter.new(Repeater.order(:call_sign, :tx_frequency))
+
+    expect(exporter.export).to eq(<<~CSV)
+      Location,Name,Frequency,Duplex,Offset,Tone,rToneFreq,cToneFreq,DtcsCode,DtcsPolarity,Mode,TStep,Skip,Comment,URCALL,RPT1CALL,RPT2CALL,DVCODE
+      0,BL4NK,144.370000,-,0.600000,,88.5,88.5,023,NN,FM,5,,Repeater BL4NK BL4NK,,,,
+      1,FU11,144.362500,-,0.600000,TONE,67.0,67.0,023,NN,FM,5,,Repeater FU11 FU11,,,,
+      2,GB3CN,145.087500,-,0.600000,TONE,94.8,94.8,023,NN,FM,5,,Newcastle Emlyn GB3CN,,,,
+      3,GB3DR,145.137500,-,0.600000,,88.5,88.5,023,NN,FM,5,,Weymouth GB3DR,,,,
+      4,GB7DC,438.475000,+,7.600000,TONE,71.9,71.9,023,NN,FM,5,,Derby GB7DC,,,,
+      5,GB7IC-C,145.062500,-,0.600000,,88.5,88.5,023,NN,FM,5,,Herne Bay GB7IC-C,,,,
+    CSV
+  end
+end
