993afaf10d663ccb450ec76bf93ad49bb9646d3d
Pablo Fernandez <pablo@pablofernandez.tech>
2024-05-11 09:27:07 +0100

    Non-semantic: Update the rspec files.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -84,7 +84,7 @@ RSpec.configure do |config|
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
-  config.example_status_persistence_file_path = "tmp/rspec_state.txt"
+  config.example_status_persistence_file_path = "spec/examples.txt"
 
   # Limits the available syntax to the non-monkey patched syntax that is
   # recommended. For more details, see:
