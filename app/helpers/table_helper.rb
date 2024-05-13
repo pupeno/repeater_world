adc0fcc1fa1840269f331a2fd0d03417ff8d3972
Pablo Fernandez <pablo@pablofernandez.tech>
2024-05-13 09:04:52 +0100

    Fix maps legend in dark mode. (#466)
    Closes #338

---
+++ app/helpers/table_helper.rb
@@ -0,0 +1,19 @@
+# Copyright 2023, Pablo Fernandez
+#
+# This file is part of Repeater World.
+#
+# Repeater World is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License.
+#
+# Repeater World is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Repeater World. If not, see
+# <https://www.gnu.org/licenses/>.
+
+module TableHelper
+  THEAD_CLASSES = "bg-gray-200 dark:bg-gray-600"
+  TH_CLASSES = "sticky top-0 z-10 border-b border-gray-300 dark:border-gray-500 px-3 py-3.5 text-left font-semibold backdrop-blur backdrop-filter"
+  TD_CLASSES = "whitespace-nowrap border-b border-gray-200 dark:border-gray-600 px-3 py-4"
+end
