58ee4bad577e3e83f774f8f70d4c3ca4bc34eb36
Pablo Fernandez <pablo@pablofernandez.tech>
2024-07-20 12:04:46 +0100

    Disable papertrail when testing except when explicitly required.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,7 @@ require_relative "../config/environment"
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
+require "paper_trail/frameworks/rspec"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
