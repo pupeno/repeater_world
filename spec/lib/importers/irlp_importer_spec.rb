9beba9702dfe72af86c8f494d93838197358d992
Pablo Fernandez <pablo@pablofernandez.tech>
2024-10-11 00:19:21 +0100

    New structure for the IRLP tests.

--- spec/lib/importers/irlp_importer_spec.rb
+++ spec/lib/importers/irlp_importer_spec.rb
@@ -13,86 +13,89 @@
 # <https://www.gnu.org/licenses/>.
 
 require "rails_helper"
+require_relative "importer_helper"
 
 RSpec.describe IrlpImporter do
-  before do
-    Repeater.destroy_all
-    files = {"https://status.irlp.net/nohtmlstatus.txt.bz2" => "irlp.tsv.bz2"}
-    files.each do |url, local_file|
-      file = double("file")
-      local_file = Rails.root.join("spec", "lib", "importers", "irlp_importer_data", local_file)
-      expect(file).to receive(:open).and_return(File.new(local_file))
-      expect(URI).to receive(:parse).with(url).and_return(file)
+  include ImporterHelper
+
+  before { Repeater.destroy_all }
+
+  context "With a good source" do
+    before do
+      double_files(
+        ["spec", "lib", "importers", "irlp_importer_data", "good"],
+        { "https://status.irlp.net/nohtmlstatus.txt.bz2" => "irlp.tsv.bz2" }
+      )
     end
-  end
 
-  it "should import" do
-    Dir.mktmpdir("IrlpImporter") do |dir|
-      expect do
-        IrlpImporter.new(working_directory: dir).import
-      end.to change { Repeater.count }.by(25)
+    it "should import" do
+      Dir.mktmpdir("IrlpImporter") do |dir|
+        expect do
+          IrlpImporter.new(working_directory: dir).import
+        end.to change { Repeater.count }.by(25)
 
-      # Grab some repeaters and verify they were imported correctly.
-      repeater = Repeater.find_sole_by(call_sign: "VE7RHS")
-      expect(repeater.name).to eq(nil)
-      expect(repeater.band).to eq(Repeater::BAND_2M)
-      expect(repeater.tx_frequency).to eq(145_270_000)
-      expect(repeater.rx_frequency).to eq(144_670_000)
+        # Grab some repeaters and verify they were imported correctly.
+        repeater = Repeater.find_sole_by(call_sign: "VE7RHS")
+        expect(repeater.name).to eq(nil)
+        expect(repeater.band).to eq(Repeater::BAND_2M)
+        expect(repeater.tx_frequency).to eq(145_270_000)
+        expect(repeater.rx_frequency).to eq(144_670_000)
 
-      # Check a case where we get multiple repeaters with the same call sign.
-      expect(Repeater.where(call_sign: "K5NX").count).to eq(2)
+        # Check a case where we get multiple repeaters with the same call sign.
+        expect(Repeater.where(call_sign: "K5NX").count).to eq(2)
+      end
     end
-  end
 
-  it "should not import anything new on a second pass" do
-    Dir.mktmpdir("IrlpImporter") do |dir|
-      IrlpImporter.new(working_directory: dir).import
-
-      # The second time we call it, it shouldn't re-download any files, nor create new repeaters
-      expect do
+    it "should not import anything new on a second pass" do
+      Dir.mktmpdir("IrlpImporter") do |dir|
         IrlpImporter.new(working_directory: dir).import
-      end.to change { Repeater.count }.by(0)
+
+        # The second time we call it, it shouldn't re-download any files, nor create new repeaters
+        expect do
+          IrlpImporter.new(working_directory: dir).import
+        end.to change { Repeater.count }.by(0)
+      end
     end
-  end
 
-  it "should respect the source values during import" do
-    Dir.mktmpdir("IrlpImporter") do |dir|
-      IrlpImporter.new(working_directory: dir).import
-
-      # This repeater simulates a previously imported repeater that is no longer in the source files, so we should
-      # delete it to avoid stale data.
-      deleted = create(:repeater, :full, call_sign: "VE7RHS", tx_frequency: 145_000_001, source: IrlpImporter.source,
-        irlp_node_number: 9999)
-
-      # This repeater represents one where the upstream data changed and should be updated by the importer.
-      changed = Repeater.find_sole_by(call_sign: "VE7RHS", irlp_node_number: 1000)
-      changed_rx_frequency_was = changed.rx_frequency
-      changed.rx_frequency = 144_000_123
-      changed.save!
-
-      # This repeater represents one that got taken over by the owner becoming a Repeater World user, that means the
-      # source is now nil. This should never again be overwritten by the importer.
-      independent = Repeater.find_sole_by(call_sign: "W7NJN")
-      independent_rx_frequency = independent.rx_frequency = 144_000_123
-      independent.source = nil
-      independent.save!
-
-      # Run the import and verify we removed one repeater but otherwise made no changes.
-      expect do
+    it "should respect the source values during import" do
+      Dir.mktmpdir("IrlpImporter") do |dir|
         IrlpImporter.new(working_directory: dir).import
-      end.to change { Repeater.count }.by(-1)
-        .and change { Repeater.where(call_sign: deleted.call_sign, tx_frequency: deleted.tx_frequency).count }.by(-1)
 
-      # This one got deleted
-      expect { deleted.reload }.to raise_error(ActiveRecord::RecordNotFound)
+        # This repeater simulates a previously imported repeater that is no longer in the source files, so we should
+        # delete it to avoid stale data.
+        deleted = create(:repeater, :full, call_sign: "VE7RHS", tx_frequency: 145_000_001, source: IrlpImporter.source,
+                         irlp_node_number: 9999)
 
-      # This got updated.
-      changed.reload
-      expect(changed.rx_frequency).to eq(changed_rx_frequency_was)
+        # This repeater represents one where the upstream data changed and should be updated by the importer.
+        changed = Repeater.find_sole_by(call_sign: "VE7RHS", irlp_node_number: 1000)
+        changed_rx_frequency_was = changed.rx_frequency
+        changed.rx_frequency = 144_000_123
+        changed.save!
 
-      # This one didn't change.
-      independent.reload
-      expect(independent.rx_frequency).to eq(independent_rx_frequency)
+        # This repeater represents one that got taken over by the owner becoming a Repeater World user, that means the
+        # source is now nil. This should never again be overwritten by the importer.
+        independent = Repeater.find_sole_by(call_sign: "W7NJN")
+        independent_rx_frequency = independent.rx_frequency = 144_000_123
+        independent.source = nil
+        independent.save!
+
+        # Run the import and verify we removed one repeater but otherwise made no changes.
+        expect do
+          IrlpImporter.new(working_directory: dir).import
+        end.to change { Repeater.count }.by(-1)
+                                        .and change { Repeater.where(call_sign: deleted.call_sign, tx_frequency: deleted.tx_frequency).count }.by(-1)
+
+        # This one got deleted
+        expect { deleted.reload }.to raise_error(ActiveRecord::RecordNotFound)
+
+        # This got updated.
+        changed.reload
+        expect(changed.rx_frequency).to eq(changed_rx_frequency_was)
+
+        # This one didn't change.
+        independent.reload
+        expect(independent.rx_frequency).to eq(independent_rx_frequency)
+      end
     end
   end
 end
